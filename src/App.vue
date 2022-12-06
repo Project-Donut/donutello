c50115e35ba575f8d0f40938efcf65feea9f7ed8
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-12-06 20:04:16 +0100

    feat: styling to vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,38 @@
 </script>
 
 <template>
+   <div class="__header">
+      <h1>Header</h1>
+    </div>
+    <div class="__userInterface">
+      <h1>User Interface</h1>
+    </div>
+    <div :ref="canvas" class="__viewPort" >
+      <h1>Canvas</h1>
+  </div>
  
 </template>
 
 <style scoped>
-
+template{
+    display: grid;
+    grid-template-columns: 4fr 8fr;
+    grid-template-rows: 200px 1fr;
+    grid-template-areas: 
+    "header viewport"
+    "ui viewport";
+}
+    .__header{
+      grid-area: header;
+      background-color: blanchedalmond;
+    }
+    .__uiserInterface{
+      grid-area: ui;
+      background-color: aquamarine;
+    }
+   .__viewPort{
+      grid-area: viewport;
+      background-color: red;
+      height: 100vh;
+    }
 </style>
