f515d106bb77562bedd5bdc5fff91cc2776e8c5a
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-12-07 11:12:19 +0100

    feat: import model from three script

--- src/scripts/three.js
+++ src/scripts/three.js
@@ -4,4 +4,27 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 //import gltf loader
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-import { onMounted,ref, render } from 'vue';
+export default class ThreeScene {
+  constructor() {
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(this.renderer.domElement)
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.loader = new GLTFLoader()
+    this.camera.position.z = 5
+    this.animate()
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate.bind(this))
+    this.renderer.render(this.scene, this.camera)
+  }
+  
+}
