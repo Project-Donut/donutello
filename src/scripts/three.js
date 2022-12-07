6c5a3656891abd4dde9fcbdb4e084201c1dde14d
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-12-07 11:18:35 +0100

    feat: update three doc

--- src/scripts/three.js
+++ src/scripts/three.js
@@ -5,26 +5,41 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export default class ThreeScene {
-  constructor() {
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
-    this.renderer = new THREE.WebGLRenderer()
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(this.renderer.domElement)
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-    this.loader = new GLTFLoader()
-    this.camera.position.z = 5
-    this.animate()
+  constructor(viewPort) {
+    this.view = viewPort;
+    this.scene;
+    this.camera;
+    this.renderer;
+    this.loader;
   }
 
-  animate() {
-    requestAnimationFrame(this.animate.bind(this))
-    this.renderer.render(this.scene, this.camera)
+  sceneSetup() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, this.view.offsetWidth / this.view.offsetHeight, 0.1, 1000);
+    this.camera.position.z = 5;
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(this.view.offsetWidth, this.view.offsetHeight);
+    this.view.appendChild(this.renderer.domElement);
   }
-  
+  modelSetup() {
+        //import donut glb
+        let donut;
+        const loader = new GLTFLoader();
+        loader.load('/bignut.glb', (gltf) => {
+            donut = gltf.scene;
+            donut.scale.set(40,40,40);
+            this.scene.add(donut);
+        });
+  }
+animate = () => {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.camera);
+  }
+  lightsSetup() {
+    const light = new THREE.AmbientLight(0x404040); // soft white light
+    this.scene.add(light);
+  }
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
 }
