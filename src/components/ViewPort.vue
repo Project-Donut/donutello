ef05c7b870117a45b984c6440fd43690b2d8f80d
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-12-07 11:05:37 +0100

    feat: oop script for three loading

--- src/components/ViewPort.vue
+++ src/components/ViewPort.vue
@@ -1,33 +1,41 @@
 <script setup>
-import * as THREE from 'three'
-//import orbit controls
-//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-//import gltf loader
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-import { onMounted,ref } from 'vue';
 
 const viewPort = ref(null);
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderScene = () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    viewPort.value.appendChild(renderer.domElement);
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    const animate = function () {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    };
+    animate();
+}
+
 onMounted(()=>{
-    const renderer = new THREE.WebGLRenderer({
-        //render in canvas
-        canvas: viewPort.value
-    });
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    
+    renderScene();
     
     //import donut glb
-    let donut;
-    const loader = new GLTFLoader();
-    loader.load('/bignut.glb', (gltf) => {
-        donut = gltf.scene;
-        donut.scale.set(40,40,40);
-        scene.add(donut);
-    });
+    //let donut;
+    //const loader = new GLTFLoader();
+    //loader.load('/bignut.glb', (gltf) => {
+    //    donut = gltf.scene;
+      //  donut.scale.set(40,40,40);
+       // scene.add(donut);
+    //});
     
-})
+});
 
 
 
@@ -35,13 +43,12 @@ onMounted(()=>{
 
 <template>
   <canvas ref="viewPort"> 
-    
   </canvas>
   <h1>test</h1>
 </template>
 
 <style scoped>
-body{
+h1{
     background-color: red;
 }
 </style>
---
+++ src/scripts/three.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three'
+//import orbit controls
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+//import gltf loader
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+import { onMounted,ref, render } from 'vue';
