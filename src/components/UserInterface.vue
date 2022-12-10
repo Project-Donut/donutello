4c7e32ea4280b0585e0c82932737693971aaee27
hippo159 <46355784+hippo159@users.noreply.github.com>
2022-12-10 11:45:02 +0100

    feat: create option for no filling

--- src/components/UserInterface.vue
+++ src/components/UserInterface.vue
@@ -6,7 +6,7 @@ let state = reactive({order: []});
 const props = defineProps(['model']);
 let icingFlavour = ref(null);
 let icing = ref(null);
-let filling = ref(null);
+let filling = ref('0xffffff');
 let fillingFlavour = ref(null);
 const updateIcing = () => {
     if(icing.value){
@@ -14,8 +14,12 @@ const updateIcing = () => {
         icingFlavour = icing.value;  
     }
 const updateFilling = () => {
-    if(filling.value){
-        props.model.loadFilling(ref(filling.value));}
+    if(filling.value && filling.value != '0x000000'){
+        props.model.loadFilling(ref(filling.value));
+        props.model.filling.visible=true;
+    }else{
+        props.model.filling.visible=false;
+    }
         fillingFlavour = filling.value;  
 }
 const createOrder = () =>{
@@ -32,22 +36,24 @@ const createOrder = () =>{
 <template>
     <div class="userInput">
         <input class="__input" type="text" placeholder="Name your nutty boy">
+        
         <label for="flavours">Choose your flavour</label>
         <select name="flavours" class="__input" v-model="icing" @click="updateIcing()">
             <option disabled value=null>Please select a flavour</option>
-            <option value="0xc71639" >
-                strawberry
-            </option>
+            <option value="0xc71639" >strawberry</option>
             <option value="0x803721">choccy</option>
             <option value="0xedc568">vanilla</option>   
         </select>
+
         <label for="filling">Fill me up baby</label>
         <select name="filling" class="__input __input-filling" v-model="filling" @click="updateFilling()">
+            <option disabled value=null>Please select a filling</option>
             <option value="0x803721">filling1</option>
             <option value="0xedc568">filling2</option>
             <option value="0xc71639">filling3</option>
-            <option value="0xffffff">No filling</option>
+            <option value="0x000000">No filling</option>
         </select>
+
         <label for="sprinkles">You want sprinkies?</label>
         <select name="sprinkles" class="__input __input-sprinkles">
             <option value="rainbow">rainbow sprinkies</option>
@@ -55,6 +61,7 @@ const createOrder = () =>{
             <option value="vanilla">vanilla sprinkies</option>
             <option value="none">No sprinkies</option>
         </select>
+
         <label for="balls">Balls?</label>
         <select name="balls" class="__input __input-balls">
             <option value="blue">Blue balls</option>
