63e8227313274aad17077c70c7293b8ad34e2090
GilianS <r0885351@student.thomasmore.be>
2022-12-13 15:23:23 +0100

    style: override default style of primevue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue';
-import './style.css';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import FileUpload from 'primevue/fileupload';
 import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 import 'primevue/resources/primevue.min.css'; 
 import 'primeicons/primeicons.css';
+import './style.css';
+import './prime_override.css';
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
 
